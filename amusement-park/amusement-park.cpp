fa11f9f0d1d5a025daf5bcf91a1c38f2c20b34d9
Caio Jordan <caioj2906@gmail.com>
2022-04-13 09:42:02 +0200

    Base files added

---
+++ amusement-park/amusement-park.cpp
@@ -0,0 +1,12 @@
+﻿// amusement-park.cpp : Defines the entry point for the application.
+//
+
+#include "amusement-park.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
